23887d4dc7c859f49ce57de10fc4b92e09ecaa41
matheus.bezerra7@hotmail.com <matheus.bezerra7@hotmail.com>
2020-11-03 22:27:35 -0300

    navbar

--- projetoIntegrador/src/app/app.module.ts
+++ projetoIntegrador/src/app/app.module.ts
@@ -5,19 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    NavComponent,
     SobreNosComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
