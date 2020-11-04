e0c13fc8f3b1276df83adab3a7dfab797262f67e
Matheus Bello <mathbello@gmail.com>
2020-11-04 01:01:35 -0300

    Junção de todas as branchs v1.0

--- projetoIntegrador/src/app/app-routing.module.ts
+++ projetoIntegrador/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { notStrictEqual } from 'assert';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { HomeComponent } from './home/home.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
   // Ao iniciar o servidor redireciona para a página home
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // Rota para a página home
+  { path: 'home', component: HomeComponent },
   // Rota para a página sobre nós
   { path: 'sobre-nos', component: SobreNosComponent },
   // Rota para a página de cadastro
