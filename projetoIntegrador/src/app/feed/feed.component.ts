084cf555503642603804ffee8d8b273a65c892e9
Matheus Bello <mathbello@gmail.com>
2020-11-18 13:15:22 -0300

    Att variaveis feed

--- projetoIntegrador/src/app/feed/feed.component.ts
+++ projetoIntegrador/src/app/feed/feed.component.ts
@@ -27,10 +27,10 @@ export class FeedComponent implements OnInit {
 
   usuario: Usuario = new Usuario();
 
-  nomeUser= localStorage.getItem("nome");
-  emailUser= localStorage.getItem("email");
-  imagemUser= localStorage.getItem("imagem");
-  idUser= Number(localStorage.getItem("id"));
+  nomeUser= environment.nome;
+  emailUser= environment.email;
+  imagemUser= environment.imagem;
+  idUser= Number(environment.id);
 
   estados = [];
   estadoId: string;
